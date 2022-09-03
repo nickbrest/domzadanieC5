3ae2e8c2e709206dcedb6eea836fbd3eb8bd2aa6
Nick <mikabrmail@gmail.com>
2022-09-03 22:44:06 +0300

    task4 complete

---
+++ task4/Program.cs
@@ -0,0 +1,30 @@
+﻿// A. Почти счастливое число. В единственной строке выведите «YES», если число n — почти счастливое, и «NO» в противном случае (без кавычек).
+// https://codeforces.com/contest/110/problem/A
+
+long number = 7747774;
+int count = 0;
+
+while (number > 0)
+{
+    if (number % 10 == 4 || number % 10 == 7) count++;
+    number /= 10;
+}
+
+if (count == 4 || count == 7)
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO");
+
+/* long number = long.Parse(Console.ReadLine());
+int count = 0;
+
+while (number > 0)
+{
+    if (number % 10 == 4 || number % 10 == 7) count++;
+    number /= 10;
+}
+
+if (count == 4 || count == 7)
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO"); */
