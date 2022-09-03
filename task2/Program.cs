09f4fa69316090de07d01b9118a010b9538fa98c
Nick <mikabrmail@gmail.com>
2022-09-03 22:43:30 +0300

    task2 complete

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// A. Слоник. За какое минимальное количество шагов он может добраться до дома друга.
+// https://codeforces.com/contest/617/problem/A
+
+int homeX = 7;
+
+int step = 0;
+if (homeX % 5 == 0) step = homeX / 5;
+else step = homeX / 5 + 1;
+
+Console.WriteLine($"Количество шагов {step}");
+
+/* int homeX = int.Parse(Console.ReadLine());
+int step = 0;
+if (homeX % 5 == 0) step = homeX / 5;
+else step = homeX / 5 + 1;
+Console.WriteLine(step); */
